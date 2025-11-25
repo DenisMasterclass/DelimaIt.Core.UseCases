28d1c8e98e4ec17cbb03b79618c77facc62b8a79
Denis Lima <128059827+DenisMasterclass@users.noreply.github.com>
2025-11-25 14:27:01 -0300

    Brazilian CNPJ Alfanumeric Validator

---
+++ Util/CnpjValidator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class CNPJValidator
+{
+    private const int TAMANHO_CNPJ_SEM_DV = 12;
+    private const string REGEX_CARACTERES_FORMATACAO = "[./-]";
+    private const string REGEX_FORMACAO_BASE_CNPJ = "[A-Z\\d]{12}";
+    private const string REGEX_FORMACAO_DV = "[\\d]{2}";
+    private const string REGEX_VALOR_ZERADO = "^[0]+$";
+
+    private const int VALOR_BASE = (int)'0';
+    private static readonly int[] PESOS_DV = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+    public static bool IsValid(string cnpj)
+    {
+        if (cnpj != null)
+        {
+            cnpj = RemoveCaracteresFormatacao(cnpj);
+            if (IsCnpjFormacaoValidaComDV(cnpj))
+            {
+                string dvInformado = cnpj.Substring(TAMANHO_CNPJ_SEM_DV);
+                string dvCalculado = CalculaDV(cnpj.Substring(0, TAMANHO_CNPJ_SEM_DV));
+                return dvCalculado.Equals(dvInformado);
+            }
+        }
+
+        return false;
+    }
+
+    public static string CalculaDV(string baseCnpj)
+    {
+        if (baseCnpj != null)
+        {
+            baseCnpj = RemoveCaracteresFormatacao(baseCnpj);
+
+            if (IsCnpjFormacaoValidaSemDV(baseCnpj))
+            {
+                string dv1 = $"{CalculaDigito(baseCnpj)}";
+                string dv2 = $"{CalculaDigito(baseCnpj + dv1)}";
+                return dv1 + dv2;
+            }
+        }
+
+        throw new ArgumentException($"Cnpj {baseCnpj} não é válido para o cálculo do DV");
+    }
+
+    private static int CalculaDigito(string cnpj)
+    {
+        int soma = 0;
+        for (int indice = cnpj.Length - 1; indice >= 0; indice--)
+        {
+            int valorCaracter = (int)cnpj[indice] - VALOR_BASE;
+            soma += valorCaracter * PESOS_DV[PESOS_DV.Length - cnpj.Length + indice];
+        }
+        return soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    }
+
+    private static string RemoveCaracteresFormatacao(string cnpj)
+    {
+        return Regex.Replace(cnpj.Trim(), REGEX_CARACTERES_FORMATACAO, "");
+    }
+
+    private static bool IsCnpjFormacaoValidaSemDV(string cnpj)
+    {
+        return Regex.IsMatch(cnpj, REGEX_FORMACAO_BASE_CNPJ) && !Regex.IsMatch(cnpj, REGEX_VALOR_ZERADO);
+    }
+
+    private static bool IsCnpjFormacaoValidaComDV(string cnpj)
+    {
+        return Regex.IsMatch(cnpj, REGEX_FORMACAO_BASE_CNPJ + REGEX_FORMACAO_DV) && !Regex.IsMatch(cnpj, REGEX_VALOR_ZERADO);
+    }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,4 @@
 ﻿Nuget for Handler Input and Output Models DelimaIt.Core.UseCases;
+Brazilian CNPJ Alfanumeric Validator;
 Free for non-commercial use;
 DeLima It;
